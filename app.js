66f36bb2800428cae9b99d1718dcba669c974b6d
Kunzete <newkunzete@gmail.com>
2024-01-15 17:11:24 +0500

    add

--- app.js
+++ app.js
@@ -77,18 +77,16 @@ audioElement.addEventListener("timeupdate",()=>{
 //Volume Changer
 
 Volume.addEventListener("change",()=>{
-    audioVolume = Volume.value / 100;
-    if ( audioVolume >= 50) {
+    audioElement.volume = Volume.value / 100;
+    if (Volume.value > 50) {
         document.getElementById("volume-icon").classList.remove("fa-volume-xmark")
-        document.getElementById("volume-icon").classList.remove("fa-volume-low")
         document.getElementById("volume-icon").classList.add("fa-volume-high")
     }
-    else if (audioVolume <= 50) {
+    else if (Volume.value < 50) {
     document.getElementById("volume-icon").classList.remove("fa-volume-high")
-    document.getElementById("volume-icon").classList.remove("fa-volume-xmark")
     document.getElementById("volume-icon").classList.add("fa-volume-low")
     }
-    else if (audioVolume = 0) {
+    else if (Volume.value < 1) {
         document.getElementById("volume-icon").classList.remove("fa-volume-high")
         document.getElementById("volume-icon").classList.remove("fa-volume-low")
         document.getElementById("volume-icon").classList.add("fa-volume-xmark")
